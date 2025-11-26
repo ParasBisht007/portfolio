d872ecde0d7e1db06828e220907f2e024ecf8e04
Paras Bisht <96820300+ParasBisht007@users.noreply.github.com>
2025-11-26 22:05:31 +0530

    script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Full Stack Developer", "Web Designer "],
+    strings: ["Full Stack Developer", "Web Designer", "Video Editor"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
